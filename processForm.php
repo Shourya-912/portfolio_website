710ea5f8cf567a028442e33fadb49e01f0d80f61
Shourya Chourasia <138221216+Shourya-912@users.noreply.github.com>
2023-10-19 00:45:22 +0530

    Update processForm.php

--- processForm.php
+++ processForm.php
@@ -1,5 +1,5 @@
 <?php
-$feedback = ""; 
+$feedback = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
@@ -7,16 +7,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = $_POST["subject"];
     $message = $_POST["message"];
 
-    // the form data processed
+    // The form data processed
     $to = "shourchourasia.912@gmail.com";
+    $subject = "Message from $name: $subject";
     $headers = "From: $email";
 
-    mail($to, $name, $subject, $message, $headers);
+    $mailSent = mail($to, $subject, $message, $headers);
 
-    // feedback message to be displayed.
-    $feedback = "Thank you! Your message has been sent.";
+    if ($mailSent) {
+        $feedback = "Thank you! Your message has been sent.";
+    } else {
+        $feedback = "Oops! Something went wrong. Please try again later.";
+    }
 }
 ?>
 
 <!-- Display the feedback message on the same page -->
 <div class="feedback"><?php echo $feedback; ?></div>
+
